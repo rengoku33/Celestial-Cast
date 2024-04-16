05bd970c2d0ca9e4aeff2a460bd93762ed81db2f
Kiran Vignesh <rajkiranvignesh369@gmail.com>
2024-04-16 12:04:08 +0530

    finished basic API fetch and styles

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
-"use client";  
+"use client";
 import Head from 'next/head';
 import axios from 'axios';
-import React,{ useState } from 'react';
-
+import React, { useState } from 'react';
+import Weather from '../components/Weather';
+import Spinner from '../components/Spinner';
 import { BsSearch } from 'react-icons/bs'
 
 export default function Home() {
-  
-  const [city,setCity] = useState('');
+
+  const [city, setCity] = useState('');
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
 
@@ -24,18 +25,48 @@ export default function Home() {
     setLoading(false);
   }
 
-  return (
-  <div>
-    <Head>
-      <title>Celestial Cast</title>
-    </Head>
-    {/* <img src={require('./images/up33.jpeg')} alt='star-forger' height={100}/> */}
-    <img
-          src='https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2575&q=80'
-          
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Celestial Cast</title>
+      </Head>
+      {/* bak grnd img */}
+      <div className="flex justify-center">
+        <img
+          src='https://raw.githubusercontent.com/rengoku33/Celestial-Cast/main/app/images/up33.jpeg'
+          className="fixed top-0 left-0 w-full h-full object-cover"
+          alt="Responsive Image"
         />
+      </div>
 
-    <button onClick={fetchWeather}>Fetch data</button>
-  </div>
-      );
+      {/* search bar */}
+      <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 text-white z-10'>
+          <form
+            onSubmit={fetchWeather}
+            className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 text-white rounded-2xl'
+          >
+            <div>
+              <input
+                onChange={(e) => setCity(e.target.value)}
+                className='bg-transparent border-none text-white focus:outline-none text-2xl'
+                type='text'
+                placeholder='Search city'
+              />
+            </div>
+            <button onClick={fetchWeather}>
+              <BsSearch size={20} />
+            </button>
+          </form>
+        </div>
+
+        {/* Weather */}
+        {weather.main && <Weather data={weather} />}
+
+    </div>
+  );
+}
 }
